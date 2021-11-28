f362504493719ea19cc6a25e03c09bf049faf6ae
mojako <magonote1195@gmail.com>
2021-11-28 11:52:59 +0900

    認証と資産残高取得APIの実装

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"bitcoin1/bitflyer"
+	"bitcoin1/config"
+	"bitcoin1/utils"
+	"fmt"
+)
 
+func main() {
+	utils.LoggingSettings(config.Config.LogFile)
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
